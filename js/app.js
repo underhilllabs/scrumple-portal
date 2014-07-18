2547d9458d372a1b0db6343e002b4c16aa2b7a5d
Bart Lantz <underhilllabs@gmail.com>
2014-07-18 12:18:06 -0600

    Adds scrumple-widget directive

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 (function() {
   var app = angular.module('scrumple', []);
 
-
-
-
+  app.directive('scrumpleWidget', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'scrumple-widget.html',
+    };
+  });
 
 })();
